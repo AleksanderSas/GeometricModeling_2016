72b53573246c156f38980358f6bd3bb5f27d2166
AleksanderSas <aleksander.sas93@gmail.com>
2016-06-23 14:25:12 +0200

    experiments

--- assignment_6/main.m
+++ assignment_6/main.m
@@ -1,17 +1,22 @@
 
 
-%fileID = fopen('ExampleData/CamelHeadSilhouette.txt','r');
+fileID = fopen('ExampleData/CamelHeadSilhouette.txt','r');
 %fileID = fopen('ExampleData/MaxPlanckSilhouette.txt','r');
-fileID = fopen('ExampleData/SineRandom.txt','r');
+%fileID = fopen('ExampleData/SineRandom.txt','r');
 A = fscanf(fileID,'%f');
 x = length(A)/ 2;
 A = reshape(A, 2, length(A)/ 2);
 fclose(fileID);
 
-B = smooth(A, 8);
+B = smooth(A, 10, 2);
 
 points = b_spline(B, 20000);
 figure(1);
 plot(A(1,:), A(2,:), 'x', 'color','b'); hold on 
-plot(B(1,:), B(2,:), 'x', 'color','g'); hold on 
-plot(points(1,:), points(2,:));
+%plot(B(1,:), B(2,:), 'x', 'color','g'); hold on 
+plot(points(1,:), points(2,:)); hold on;
+
+B = smooth(A, 14, 2);
+points = b_spline(B, 20000);
+figure(1);
+plot(points(1,:), points(2,:), 'color', 'r');
--- assignment_6/smooth.m
+++ assignment_6/smooth.m
@@ -1,4 +1,4 @@
-function data_out = smooth(data_in, window_size)
+function data_out = smooth(data_in, window_size, degree)
     data_out = zeros(0,0);
     step = window_size / 2;
     i = 1;
@@ -7,10 +7,11 @@ function data_out = smooth(data_in, window_size)
     
     t = linspace(1, 10, window_size);
     points = [3.75,6.25];
-    while i + step < size(data_in, 2)
+    while i + window_size-1 < size(data_in, 2)
         subdata = data_in(:,i:i+window_size-1);
         
-        data_out = [data_out,[poli_approximation(t, subdata(1,:), points ,2); poli_approximation(t, subdata(2,:), points ,2)]];
+        data_out = [data_out,[poli_approximation(t, subdata(1,:), points ,degree);...
+            poli_approximation(t, subdata(2,:), points ,degree)]];
         
         i = i + step;
         k = k + step2;
